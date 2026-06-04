5a92fabc873d368055d78e119314502f02244169
Vera Goetze <vera.goetze@gmail.com>
2026-06-04 12:50:33 -0700

    inverted deprivation - fixed output tables

--- 04_Statistical Analysis.R
+++ 04_Statistical Analysis.R
@@ -61,7 +61,7 @@ trans_grid <- dens_grid %>%
     dich_sfh = as.numeric(ifelse(sfh_share > 0.5, 1, 0)),
     
     #deprivation
-    scaled_deprivation = scale(deprivation),
+    scaled_deprivation = -scale(deprivation),
 
     #output area classification
     oac_constrained = as.numeric(ifelse(SPRGRP == "7",1,0)),
@@ -90,6 +90,30 @@ trans_grid <- dens_grid %>%
 sapply(trans_grid,function(x) sum(is.na(x))) #all NA values are because the cell did not have any address data at any point
 trans_grid <- na.omit(trans_grid)
 
+
+#Variable label mapping and display order----
+term_labels <- c(
+  "(Intercept)"         = "(Intercept)",
+  "scaled_logamenities" = "(ln) Amenity count",
+  "scaled_disttrain"    = "Distance to train station",
+  "scaled_distcenter"   = "Distance to Liverpool centre",
+  "scaled_hd_nb"        = "High-density cells nearby",
+  "scaled_ld_nb"        = "Low-density cells nearby",
+  "scaled_ub_nb"        = "Unbuilt cells nearby",
+  "scaled_deprivation"  = "Deprivation decile",
+  "oac_constrained"     = "OAC Supergroup Constrained City Dwellers",
+  "oac_cosmopolitan"    = "OAC Supergroup Cosmopolitans",
+  "oac_ethnicentral"    = "OAC Supergroup Ethnicity Central",
+  "oac_hardpressed"     = "OAC Supergroup Hard-Pressed Living",
+  "oac_suburban"        = "OAC Supergroup Suburbanites",
+  "scaled_logdensity"   = "(ln) Address density 2013",
+  "share_pre1919bld"    = "Share of buildings constructed pre-1919",
+  "dich_sfh"            = "Single-family housing dominant",
+  "AUC"                 = "AUC",
+  "F1"                  = "F1",
+  "McFadden_R2"         = "McFadden_R2"
+)
+term_order <- unname(term_labels)
 
 #Helper function to calculate optimal f1----
 calculate_optimal_f1 <- function(pred_probs, actual_densification) {
@@ -133,65 +157,88 @@ roc_obj <- roc(plain_grid$densification, pred)
 auc(roc_obj)
 calculate_optimal_f1(pred, plain_grid$densification)
 
+#Plain model results for table----
+plain_auc <- as.numeric(auc(roc_obj))
+plain_r2  <- pR2(model_plain)["McFadden"]
+plain_f1  <- calculate_optimal_f1(pred, plain_grid$densification)$f1
+
+plain_results <- bind_rows(
+  tidy(model_plain) %>%
+    filter(p.value < 0.05) %>%
+    select(term, estimate) %>%
+    mutate(term = unname(term_labels[term])),
+  tibble(term = c("AUC", "F1", "McFadden_R2"),
+         estimate = c(plain_auc, plain_f1, plain_r2))
+) %>%
+  mutate(model = "densification")
+
 
 #Run models for densification types ----
 dependent_vars <- c("subdivision", "hmo", "office_rental", "large_mfh", "small_mfh", "large_sfh", "small_sfh")
 
 all_results <- map_dfr(dependent_vars, function(dep) {
-  
+
   df <- trans_grid %>%
     filter(densification == 1) %>%
     dplyr::select(-c(densification, hmo, subdivision, office_rental,
                      large_mfh, large_sfh, small_mfh, small_sfh))
   df <- bind_cols(df, trans_grid %>% filter(densification == 1) %>% select(all_of(dep))) #reattach dependent variable
-  
+
   names(df)[ncol(df)] <- dep #renames last column to current dependent variable name
-  
+
   model <- glm(reformulate(" . - grid_id", dep), data = df, family = "binomial")
-  
+
   pred <- predict(model, type = "response")
   roc_obj <- roc(df[[dep]], pred)
   auc_val <- as.numeric(auc(roc_obj))
   r2_val <- pR2(model)["McFadden"]
   f1_val <- calculate_optimal_f1(pred, df[[dep]])$f1
-  
+
   coef_df <- tidy(model) %>%
     filter(p.value < 0.05) %>%
-    select(term, estimate)
-  
+    select(term, estimate) %>%
+    mutate(term = unname(term_labels[term]))
+
   metrics <- tibble(
-    term = c("AUC", "McFadden_R2", "F1"),
-    estimate = c(auc_val, r2_val, f1_val)
+    term = c("AUC", "F1", "McFadden_R2"),
+    estimate = c(auc_val, f1_val, r2_val)
   )
-  
+
   bind_rows(coef_df, metrics) %>%
     mutate(model = dep)
 })
 
-# Order terms: coefficients first, indicators last ----
+all_results <- bind_rows(plain_results, all_results)
 
-indicator_order <- c("AUC", "McFadden_R2", "F1")
+# Order terms according to term_order; indicators sit at the end ----
+
+indicator_order <- c("AUC", "F1", "McFadden_R2")
+display_vars <- c("densification", dependent_vars)
 
 wide_results <- all_results %>%
   mutate(
-    estimate = round(estimate, 3),
-    is_indicator = term %in% indicator_order
+    estimate = round(estimate, 2),
+    is_indicator = term %in% indicator_order,
+    term = factor(term, levels = term_order)
   ) %>%
-  arrange(is_indicator, term) %>%
-  pivot_wider(names_from = model, values_from = estimate)
+  arrange(term) %>%
+  pivot_wider(names_from = model, values_from = estimate) %>%
+  select(term, is_indicator, all_of(display_vars))
 
 # Build formatted flextable----
 
-ft <- flextable(wide_results) %>%
+ft <- flextable(wide_results,
+                col_keys = setdiff(names(wide_results), "is_indicator")) %>%
   set_header_labels(term = "Variable / Indicator")
 
 # Color coefficients only (exclude indicators)
 coef_rows <- which(!wide_results$is_indicator)
 
-for (col in dependent_vars) {
-  ft <- color(ft, i = coef_rows[wide_results[[col]][coef_rows] > 0],
+for (col in display_vars) {
+  vals <- wide_results[[col]][coef_rows]
+  ft <- color(ft, i = coef_rows[which(vals > 0)],
               j = col, color = "green")
-  ft <- color(ft, i = coef_rows[wide_results[[col]][coef_rows] < 0],
+  ft <- color(ft, i = coef_rows[which(vals < 0)],
               j = col, color = "red")
 }
 
@@ -237,42 +284,49 @@ all_results <- map_dfr(dependent_vars, function(dep) {
   
   coef_df <- tidy(model) %>%
     filter(p.value < 0.05) %>%
-    select(term, estimate)
-  
+    select(term, estimate) %>%
+    mutate(term = unname(term_labels[term]))
+
   metrics <- tibble(
-    term = c("AUC", "McFadden_R2", "F1"),
-    estimate = c(auc_val, r2_val, f1_val)
+    term = c("AUC", "F1", "McFadden_R2"),
+    estimate = c(auc_val, f1_val, r2_val)
   )
-  
+
   bind_rows(coef_df, metrics) %>%
     mutate(model = dep)
 })
 
-# Order terms: coefficients first, indicators last ----
+all_results <- bind_rows(plain_results, all_results)
 
-indicator_order <- c("AUC", "McFadden_R2", "F1")
+# Order terms according to term_order; indicators sit at the end ----
+
+indicator_order <- c("AUC", "F1", "McFadden_R2")
+display_vars <- c("densification", dependent_vars)
 
 wide_results <- all_results %>%
   mutate(
-    estimate = round(estimate, 3),
-    is_indicator = term %in% indicator_order
+    estimate = round(estimate, 2),
+    is_indicator = term %in% indicator_order,
+    term = factor(term, levels = term_order)
   ) %>%
-  arrange(is_indicator, term) %>%
-  pivot_wider(names_from = model, values_from = estimate)%>% 
-  select(-is_indicator)
+  arrange(term) %>%
+  pivot_wider(names_from = model, values_from = estimate) %>%
+  select(term, is_indicator, all_of(display_vars))
 
 # Build formatted flextable----
 
-ft <- flextable(wide_results) %>%
+ft <- flextable(wide_results,
+                col_keys = setdiff(names(wide_results), "is_indicator")) %>%
   set_header_labels(term = "Variable / Indicator")
 
 # Color coefficients only (exclude indicators)
 coef_rows <- which(!wide_results$is_indicator)
 
-for (col in dependent_vars) {
-  ft <- color(ft, i = coef_rows[wide_results[[col]][coef_rows] > 0],
+for (col in display_vars) {
+  vals <- wide_results[[col]][coef_rows]
+  ft <- color(ft, i = coef_rows[which(vals > 0)],
               j = col, color = "green")
-  ft <- color(ft, i = coef_rows[wide_results[[col]][coef_rows] < 0],
+  ft <- color(ft, i = coef_rows[which(vals < 0)],
               j = col, color = "red")
 }
 
